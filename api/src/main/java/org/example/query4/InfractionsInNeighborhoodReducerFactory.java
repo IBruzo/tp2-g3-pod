22a6d3d92aaa2163549504b5d0cdf90f7d052732
meursault00 <inakibengolea@gmail.com>
2024-06-09 16:09:09 -0300

    Improve performance on Q4

--- api/src/main/java/org/example/query4/InfractionsInNeighborhoodReducerFactory.java
+++ api/src/main/java/org/example/query4/InfractionsInNeighborhoodReducerFactory.java
@@ -4,30 +4,36 @@ import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 import org.example.models.Pair;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SuppressWarnings("deprecation")
-public class InfractionsInNeighborhoodReducerFactory implements ReducerFactory<Pair<String,String>, Integer, Integer> {
+public class InfractionsInNeighborhoodReducerFactory implements ReducerFactory<String, Pair<String,Integer>, Map<String,Integer>> {
     @Override
-    public Reducer<Integer, Integer> newReducer(Pair<String,String> key) {
+    public Reducer<Pair<String,Integer>, Map<String,Integer>> newReducer(String key) {
         return new InfractionsInNeighborhoodReducer();
     }
 
-    private class InfractionsInNeighborhoodReducer extends Reducer<Integer, Integer> {
-        private int sum;
+    private class InfractionsInNeighborhoodReducer extends Reducer<Pair<String,Integer>, Map<String,Integer>> {
+        private Map<String,Integer> result = new HashMap<>();
 
         @Override
         public void beginReduce() {
-            System.out.println("Reducer created");
-            sum = 0;
+            result = new HashMap<>();
         }
 
         @Override
-        public void reduce(Integer value) {
-            sum += value;
+        public void reduce(Pair<String,Integer> value) {
+            if (result.containsKey(value.getFirst())) {
+                result.put(value.getFirst(), result.get(value.getFirst()) + value.getSecond());
+            } else {
+                result.put(value.getFirst(), value.getSecond());
+            }
         }
 
         @Override
-        public Integer finalizeReduce() {
-            return sum;
+        public Map<String, Integer> finalizeReduce() {
+            return result;
         }
     }
 }
